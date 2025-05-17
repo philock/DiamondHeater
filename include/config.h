f2a1d02d26553c18e1f6ec078c61fda3c7493a73
philipp <philipp@email.com>
2025-05-17 15:39:34 +0200

    All PCB subsystems integrated and tested. No PID or communication.

---
+++ include/config.h
@@ -0,0 +1,31 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Circuit parameters
+#define R_SENSE   0.025    // Current sense resistor (Ohms)
+#define I_GAIN    26.45455 // Gain of the current feedback amplifier (1 + R1/R2)
+#define R_REF     4000.0   // Value of the Rref resistor. 4300.0 for PT1000
+#define R_NOMINAL 1000.0   // 'Nominal' 0 °C resistance of the RTD sensor
+#define V_REF     3.81     // DAC reference voltage
+
+// Maximum values
+#define I_MAX 5 // Maximum output current (A)
+#define T_MAX 200 // Maximum temperature (°C)
+
+// Control loop PID parameters
+#define PIDKp 1.0
+#define PIDKi 1.0
+#define PIDKd 1.0
+
+// Pin definitions
+#define PIN_FAULT   4
+#define PIN_OC      3
+#define PIN_OT      2
+#define PIN_ACTIVE  1
+#define PIN_POWER   0
+#define PIN_CS      10
+#define PIN_SYNC    9
+#define PIN_RESET   17
+#define PIN_ADC     15
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,187 @@
 #include <Arduino.h>
+#include <Adafruit_MAX31865.h>
+#include <led.h>
+#include <input.h>
+#include <ADC.h>
+#include <config.h>
+#include <PID_v2.h>
+#include <DACx311.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+Input buttonReset(PIN_RESET, false, true);
+
+LED ledFault(PIN_FAULT);
+LED ledOC(PIN_OC);
+LED ledOT(PIN_OT);
+LED ledActive(PIN_ACTIVE);
+LED ledPower(PIN_POWER);
+
+Adafruit_MAX31865 thermo(PIN_CS, &SPI);
+ADC *adc = new ADC();
+DACxx11 dac;
+
+PID_v2 pid(PIDKp, PIDKi, PIDKd, PID::Direction::Direct);
+
+elapsedMillis systemTick;
+
+void resetSystem();
+
+void initButton(){
+    buttonReset.limitRate(100);
+    buttonReset.setDebounceTime(200);
+    buttonReset.setActivationHandler(resetSystem);
+}
+
+void initADCDAC(){
+    // ADC configuration for high accuracy
+    adc->adc0->setResolution(12);
+    adc->adc0->setAveraging(32);
+    adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::LOW_SPEED); 
+    adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::LOW_SPEED); 
+
+    adc->adc0->calibrate();
+    adc->adc0->wait_for_cal();
+
+    // initialize DAC
+    dac.begin(PIN_SYNC, DAC7311, V_REF);
+    dac.setVoltage(0);
+}
+
+void initLEDs(){
+    const int T = 100; // LED test flash speed in ms
+
+    ledFault.on();
+    delay(T);
+
+    ledOC.on();
+    delay(T);
+
+    ledOT.on();
+    ledFault.off();
+    delay(T);
+
+    ledActive.on();
+    ledOC.off();
+    delay(T);
+
+    ledPower.on();
+    ledOT.off();
+    delay(T);
+
+    ledActive.off();
+    delay(T);
+
+    ledPower.off();
+    delay(3*T);
+
+    ledPower.on();
+}
+
+void initPID(){
+    pid.SetOutputLimits(0, 4095); // Output limited by 12-bit DAC
+    
+}
+
+void resetSystem(){
+    Serial.println("Resetting...");
+    ledPower.off();
+    //USB1_USBCMD = 0; // disconnect USB
+    //delay(50);       // time for USB hubs/ports to detect disconnect
+    SCB_AIRCR = 0x05FA0004; // Application Interrupt and Reset Control Register
+}
+
+void overCurrentProtection(){
+    if(adc->adc0->isComplete()){
+        constexpr float convFactor = (3.3 / (4095 * I_GAIN * R_SENSE));
+
+        // Read analog value from ADC and convert to current in amps
+        int val = adc->adc0->analogRead(PIN_ADC);
+        float current = val * convFactor; 
+
+        if(current > I_MAX){
+            ledOC.on();
+        }
+    }
+    else{
+        adc->adc0->startSingleRead(A1);
+    }
+}
+
+void printThermoFaults(){
+    uint8_t fault = thermo.readFault();
+    if (fault) {
+        Serial.print("Fault 0x"); Serial.println(fault, HEX);
+        if (fault & MAX31865_FAULT_HIGHTHRESH) {
+        Serial.println("RTD High Threshold"); 
+        }
+        if (fault & MAX31865_FAULT_LOWTHRESH) {
+        Serial.println("RTD Low Threshold"); 
+        }
+        if (fault & MAX31865_FAULT_REFINLOW) {
+        Serial.println("REFIN- > 0.85 x Bias"); 
+        }
+        if (fault & MAX31865_FAULT_REFINHIGH) {
+        Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
+        }
+        if (fault & MAX31865_FAULT_RTDINLOW) {
+        Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+        }
+        if (fault & MAX31865_FAULT_OVUV) {
+        Serial.println("Under/Over voltage"); 
+        }
+        thermo.clearFault();
+    }
+}
+
+void setCurrent(float I){
+    float U = I_GAIN*R_SENSE*I;
+    dac.setVoltage(U);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    initButton();
+    initADCDAC();
+    initPID();
+    initLEDs();
+
+    thermo.begin(MAX31865_4WIRE);  // set to 2WIRE or 4WIRE as necessary
+    thermo.enable50Hz(true);
+
+    Serial.begin(115200);
+
+    ledActive.blink();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    buttonReset.poll();
+    ledActive.poll();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if(systemTick > 100){
+        //overCurrentProtection();
+        //Serial.println(thermo.temperature(R_NOMINAL, R_REF));
+        systemTick = 0;
+    }
+
+    /* for(float i = 0; i <= 0.41; i += 0.05){
+        setCurrent(i);
+        delay(1500);
+        Serial.println(adc->adc0->analogRead(PIN_ADC)*(3.3 / (4095 * I_GAIN * R_SENSE)));
+    }
+    for(float i = 0.4; i >= -0.01; i -= 0.05){
+        setCurrent(i);
+        Serial.println(adc->adc0->analogRead(PIN_ADC)*(3.3 / (4095 * I_GAIN * R_SENSE)));
+        delay(1500);
+    } */
+
+    const float I[] = {1.0, 2.0, 3.0, 4.0};
+    for(unsigned int i = 0; i < sizeof(I)/sizeof(float); i++){
+        //Serial.println(I[i]);
+        setCurrent(I[i]);
+        delay(100);
+        Serial.println(adc->adc0->analogRead(PIN_ADC)*(3.3 / (4095 * I_GAIN * R_SENSE)));
+        delay(50);
+        setCurrent(0);
+        delay(15);
+    }
+
 }
